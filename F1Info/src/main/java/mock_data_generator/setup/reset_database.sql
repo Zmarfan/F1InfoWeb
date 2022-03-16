e41db52885631f1d0a502bfc68feb57592dedaa1
Filip Petersson <zmarfan@hotmail.com>
2022-03-16 21:57:46 +0100

    Rewrote how resetting of database works as to not database entirely

--- F1Info/src/main/java/mock_data_generator/setup/reset_database.sql
+++ F1Info/src/main/java/mock_data_generator/setup/reset_database.sql
@@ -1,3 +1,16 @@
-drop database f1database;
-create database f1database;
+set foreign_key_checks = 0;
+set @tables = null;
+select
+  group_concat('`', table_schema, '`.`', table_name, '`') into @tables
+from
+  information_schema.tables
+where
+  table_schema = 'f1database';
+
+set @tables = concat('drop table ', @tables);
+prepare stmt from @tables;
+execute stmt;
+deallocate prepare stmt;
+set foreign_key_checks = 1;
+
 set autocommit='OFF';
--- F1Info/src/main/java/mock_data_generator/tables/01_base_tables.sql
+++ F1Info/src/main/java/mock_data_generator/tables/01_base_tables.sql
@@ -1,6 +1,6 @@
 create table countries(
-    country_code varchar(255) not null,
-    country_name varchar(255) not null,
+  country_code varchar(255) not null,
+  country_name varchar(255) not null,
 
-    constraint countries_pk primary key (country_code)
+  constraint countries_pk primary key (country_code)
 );
