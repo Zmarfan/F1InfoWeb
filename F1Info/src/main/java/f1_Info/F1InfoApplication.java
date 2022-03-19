99805469968535d6ec34c11c894a20a9e636a5f5
Filip Petersson <zmarfan@hotmail.com>
2022-03-19 20:10:12 +0100

    Created a cron job to run data fetcher

--- F1Info/src/main/java/f1_Info/F1InfoApplication.java
+++ F1Info/src/main/java/f1_Info/F1InfoApplication.java
@@ -1,15 +1,13 @@
 package f1_Info;
 
-import f1_Info.ergast.ErgastProxy;
-import f1_Info.ergast.responses.ConstructorData;
+import f1_Info.background.DataFetchingTask;
+import f1_Info.logger.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-import javax.annotation.PostConstruct;
-import java.util.List;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 public class F1InfoApplication extends SpringBootServletInitializer {
@@ -18,27 +16,20 @@ public class F1InfoApplication extends SpringBootServletInitializer {
 	}
 
 	@Autowired
-	private ErgastProxy mErgastProxy;
+	private DataFetchingTask mDataFetchingTask;
+
+	@Autowired
+	private Logger mLogger;
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(F1InfoApplication.class);
 	}
 
-	@PostConstruct
-	public void listen() {
-		final List<ConstructorData> constructors = mErgastProxy.fetchAllConstructors();
-		final List<String> insertStatements = constructors.stream().map(this::constructorToInsertStatement).toList();
-		System.out.println(String.join("\n", insertStatements));
-	}
-
-	private String constructorToInsertStatement(final ConstructorData constructorData) {
-		return String.format(
-			"insert into constructors (constructor_identifier, name, country_code, wikipedia_page) values ('%s', '%s', '%s', '%s');",
-			constructorData.getConstructorIdentifier(),
-			constructorData.getName(),
-			constructorData.getCountry().getCode(),
-			constructorData.getWikipediaUrl()
-		);
+	@Scheduled(cron = "*/5 * * * * *")
+	public void runDataFetchingTask() {
+		mLogger.logInfo("Started Data fetching task");
+		mDataFetchingTask.run();
+		mLogger.logInfo("Finished Data fetching task");
 	}
 }
---
+++ F1Info/src/main/java/f1_Info/background/DataFetchingTask.java
@@ -0,0 +1,41 @@
+package f1_Info.background;
+
+import f1_Info.ergast.ErgastProxy;
+import f1_Info.ergast.responses.ConstructorData;
+import f1_Info.logger.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DataFetchingTask {
+    private final ErgastProxy mErgastProxy;
+    private final Logger mLogger;
+
+    @Autowired
+    public DataFetchingTask(ErgastProxy ergastProxy, Logger logger) {
+        mErgastProxy = ergastProxy;
+        mLogger = logger;
+    }
+
+    public void run() {
+        try {
+            final List<ConstructorData> constructors = mErgastProxy.fetchAllConstructors();
+            final List<String> insertStatements = constructors.stream().map(this::constructorToInsertStatement).toList();
+            System.out.println(String.join("\n", insertStatements));
+        } catch (final Exception e) {
+            mLogger.logError("Failed to complete Data Fetching Task", e);
+        }
+    }
+
+    private String constructorToInsertStatement(final ConstructorData constructorData) {
+        return String.format(
+            "insert into constructors (constructor_identifier, name, country_code, wikipedia_page) values ('%s', '%s', '%s', '%s');",
+            constructorData.getConstructorIdentifier(),
+            constructorData.getName(),
+            constructorData.getCountry().getCode(),
+            constructorData.getWikipediaUrl()
+        );
+    }
+}
