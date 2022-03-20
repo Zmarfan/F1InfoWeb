67c088a965441beca27aa3f39d0a4f11ca8f838c
Filip Petersson <zmarfan@hotmail.com>
2022-03-20 22:17:15 +0100

    Added timestamp to logs

--- F1Info/src/main/java/f1_Info/logger/Logger.java
+++ F1Info/src/main/java/f1_Info/logger/Logger.java
@@ -2,20 +2,28 @@ package f1_Info.logger;
 
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Component
 public class Logger {
-    private static final String INFO_TEMPLATE = "%s in method: %s in class: %s";
-    private static final String ERROR_TEMPLATE = "%s in method: %s in class: %s with exception: %s with the stacktrace: ";
-    private static final String STACKTRACE_TEMPLATE = "Stack trace:\n%s";
+    private static final String INFO_TEMPLATE = "%s: %s in method: %s in class: %s";
+    private static final String ERROR_TEMPLATE = "%s: %s in method: %s in class: %s with exception: %s with the stacktrace: ";
 
     // TODO: Make proper logging
     public <T> void logInfo(final String methodName, final Class<T> classType, final String message) {
-        System.out.println(String.format(INFO_TEMPLATE, message, methodName, classType.getName()));
+        System.out.println(String.format(INFO_TEMPLATE, getFormattedTimestamp(), message, methodName, classType.getName()));
     }
 
     // TODO: Make proper logging
     public <T> void logError(final String methodName, final Class<T> classType, final String message, final Exception exception) {
-        System.out.println(String.format(ERROR_TEMPLATE, message, methodName, classType.getName(), exception));
+        System.out.println(String.format(ERROR_TEMPLATE, getFormattedTimestamp(), message, methodName, classType.getName(), exception));
         exception.printStackTrace();
+    }
+
+    private String getFormattedTimestamp() {
+        final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        final LocalDateTime now = LocalDateTime.now();
+        return dateTimeFormatter.format(now);
     }
 }
