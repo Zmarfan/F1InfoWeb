915dbc2e5922caa1241d0ed7e4ba08b526489ea9
Filip Petersson <zmarfan@hotmail.com>
2022-03-20 20:47:01 +0100

    Added more configuration for accessing database

--- F1Info/src/main/java/f1_Info/configuration/ConfigurationRules.java
+++ F1Info/src/main/java/f1_Info/configuration/ConfigurationRules.java
@@ -5,9 +5,20 @@ import lombok.Value;
 
 @Value
 public class ConfigurationRules {
+    String databaseUrl;
+    String databaseName;
+    String localDatabasePassword;
     boolean isMock;
 
-    public ConfigurationRules(@JsonProperty("isMock") final boolean isMock) {
+    public ConfigurationRules(
+        @JsonProperty("databaseUrl") final String databaseUrl,
+        @JsonProperty("databaseName") final String databaseName,
+        @JsonProperty("localDatabasePassword") final String localDatabasePassword,
+        @JsonProperty("isMock") final boolean isMock
+    ) {
+        this.databaseUrl = databaseUrl;
+        this.databaseName = databaseName;
+        this.localDatabasePassword = localDatabasePassword;
         this.isMock = isMock;
     }
 }
