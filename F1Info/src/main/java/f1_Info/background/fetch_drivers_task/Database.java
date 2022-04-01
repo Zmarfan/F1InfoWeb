e25ce878eda3075031d901c287d09e3160c6b883
Filip Petersson <zmarfan@hotmail.com>
2022-04-01 19:40:00 +0200

    Fixed so that driverdata actually gets merged if pulling duplicates

--- F1Info/src/main/java/f1_Info/background/fetch_drivers_task/Database.java
+++ F1Info/src/main/java/f1_Info/background/fetch_drivers_task/Database.java
@@ -28,22 +28,34 @@ public class Database extends TaskDatabase {
     public void mergeIntoDriversData(final List<DriverData> driverDataList) throws SQLException {
         try (final Connection connection = getConnection()) {
             for (final DriverData driverData : driverDataList) {
-                try (final PreparedStatement preparedStatement = connection.prepareStatement("""
-                    insert into drivers (driver_identifier, number, code, first_name, last_name, date_of_birth, country_code, wikipedia_page)
-                       values (?,?,?,?,?,?,?,?) on duplicate key update id = id;
-                    """
-                )) {
-                    preparedStatement.setString(1, driverData.getDriverIdentifier());
-                    setNullableInt(preparedStatement, 2, driverData.getPermanentNumber().orElse(null));
-                    setNullableString(preparedStatement, 3, driverData.getCode().orElse(null));
-                    preparedStatement.setString(4, driverData.getFirstName());
-                    preparedStatement.setString(5, driverData.getLastName());
-                    setDate(preparedStatement, 6, driverData.getDateOfBirth());
-                    setCountry(preparedStatement, 7, driverData.getCountry());
-                    setUrl(preparedStatement, 8, driverData.getWikipediaUrl());
-                    preparedStatement.executeUpdate();
+                if (driverDataAlreadyExistInDatabase(driverData, connection)) {
+                    insertDriverDataEntry(driverData, connection);
                 }
             }
+        }
+    }
+
+    private boolean driverDataAlreadyExistInDatabase(final DriverData driverData, final Connection connection) throws SQLException {
+        try (final PreparedStatement preparedStatement = connection.prepareStatement("select id from drivers where driver_identifier = ?;")) {
+            preparedStatement.setString(1, driverData.getDriverIdentifier());
+            return preparedStatement.executeQuery().next();
+        }
+    }
+
+    private void insertDriverDataEntry(final DriverData driverData, final Connection connection) throws SQLException {
+        try (final PreparedStatement preparedStatement = connection.prepareStatement("""
+            insert into drivers (driver_identifier, number, code, first_name, last_name, date_of_birth, country_code, wikipedia_page) values (?,?,?,?,?,?,?,?);
+            """
+        )) {
+            preparedStatement.setString(1, driverData.getDriverIdentifier());
+            setNullableInt(preparedStatement, 2, driverData.getPermanentNumber().orElse(null));
+            setNullableString(preparedStatement, 3, driverData.getCode().orElse(null));
+            preparedStatement.setString(4, driverData.getFirstName());
+            preparedStatement.setString(5, driverData.getLastName());
+            setDate(preparedStatement, 6, driverData.getDateOfBirth());
+            setCountry(preparedStatement, 7, driverData.getCountry());
+            setUrl(preparedStatement, 8, driverData.getWikipediaUrl());
+            preparedStatement.executeUpdate();
         }
     }
 }
