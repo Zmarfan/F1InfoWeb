fe3fe35582ea043e57ce0339bbf3e3d1fd47a8d7
Filip Petersson <zmarfan@hotmail.com>
2022-04-08 13:20:33 +0200

    Utilized query datas for race merging and fetching

--- F1Info/src/main/java/f1_Info/background/fetch_races_task/Database.java
+++ F1Info/src/main/java/f1_Info/background/fetch_races_task/Database.java
@@ -2,16 +2,15 @@ package f1_Info.background.fetch_races_task;
 
 import f1_Info.background.TaskDatabase;
 import f1_Info.configuration.Configuration;
+import f1_Info.database.DatabaseBulkOfWork;
 import f1_Info.ergast.responses.race.RaceData;
 import f1_Info.logger.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.sql.*;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
-
-import static f1_Info.database.StatementHelper.*;
 
 @Component(value = "FetchRacesTaskDatabase")
 public class Database extends TaskDatabase {
@@ -27,116 +26,17 @@ public class Database extends TaskDatabase {
     }
 
     public Optional<Integer> getNextSeasonToFetchForRaces() throws SQLException {
-        try (final Connection connection = getConnection()) {
-            try (final PreparedStatement preparedStatement = connection.prepareStatement("""
-                select ifnull((select
-                  case when seasons.year is not null then history.season + 1 else -1 end as next_season_to_fetch
-                from
-                  races_fetching_history history
-                    left join seasons on seasons.year = history.season + 1
-                where
-                    is_active = 'Y'), ?) as next_fetch_year;
-            """)) {
-                preparedStatement.setInt(1, FIRST_FORMULA_ONE_SEASON);
-                final ResultSet resultSet = preparedStatement.executeQuery();
-                resultSet.next();
-                return Optional.empty();
-                //return readNullableInt(resultSet, 1).filter(nextSeason -> nextSeason != NO_MORE_DATA_CAN_BE_FETCHED);
-            }
-        }
+        final Integer fetchedSeason = executeBasicQuery(new GetNextSeasonToFetchForRacesQueryData(FIRST_FORMULA_ONE_SEASON));
+        return Optional.ofNullable(fetchedSeason).filter(season -> season != NO_MORE_DATA_CAN_BE_FETCHED);
     }
 
     public void mergeIntoRacesData(final List<RaceData> raceDataList) throws SQLException {
-        try (final Connection connection = getConnection()) {
-            for (final RaceData raceData : raceDataList) {
-                if (!raceDataAlreadyExistInDatabase(raceData, connection)) {
-                    insertRaceDataEntry(raceData, connection);
-                }
-            }
-        }
+        final DatabaseBulkOfWork bulkOfWork = new DatabaseBulkOfWork();
+        bulkOfWork.add(raceDataList.stream().map(MergeIntoRacesQueryData::new).toList());
+        executeBulkOfWork(bulkOfWork);
     }
 
     public void setLastFetchedSeason(final int lastFetchedSeason) throws SQLException {
-        try (final Connection connection = getConnection()) {
-            try (final PreparedStatement preparedStatement = connection.prepareStatement("""
-                update races_fetching_history set is_active = 'N' where is_active = 'Y';
-            """)) {
-                preparedStatement.executeUpdate();
-            }
-            try (final PreparedStatement preparedStatement = connection.prepareStatement("""
-                insert into races_fetching_history (season, is_active) values (?,'Y');
-            """)) {
-                preparedStatement.setInt(1, lastFetchedSeason);
-                preparedStatement.executeUpdate();
-            }
-        }
-    }
-
-    private boolean raceDataAlreadyExistInDatabase(final RaceData raceData, final Connection connection) throws SQLException {
-        try (final PreparedStatement preparedStatement = connection.prepareStatement("select id from races where year = ? and round = ?;")) {
-            preparedStatement.setInt(1, raceData.getYear());
-            preparedStatement.setInt(2, raceData.getRound());
-            return preparedStatement.executeQuery().next();
-        }
-    }
-
-    private void insertRaceDataEntry(final RaceData raceData, final Connection connection) throws SQLException {
-        final Optional<Integer> raceTimeAndDateId = insertTimeAndDate(raceData.getRaceTime(), raceData.getRaceDate(), connection);
-        final Optional<Integer> qualifyingTimeAndDateId = insertTimeAndDate(raceData.getQualifyingTime(), raceData.getQualifyingDate(), connection);
-        final Optional<Integer> sprintTimeAndDateId = insertTimeAndDate(raceData.getSprintTime(), raceData.getSprintDate(), connection);
-        final Optional<Integer> fp1TimeAndDateId = insertTimeAndDate(raceData.getFirstPracticeTime(), raceData.getFirstPracticeDate(), connection);
-        final Optional<Integer> fp2TimeAndDateId = insertTimeAndDate(raceData.getSecondPracticeTime(), raceData.getSecondPracticeDate(), connection);
-        final Optional<Integer> fp3TimeAndDateId = insertTimeAndDate(raceData.getThirdPracticeTime(), raceData.getThirdPracticeDate(), connection);
-
-        try (final PreparedStatement preparedStatement = connection.prepareStatement("""
-            insert into races(
-                circuit_id,
-                year,
-                round,
-                name,
-                race_time_and_date_id,
-                qualifying_time_and_date_id,
-                sprint_time_and_date_id,
-                first_practice_time_and_date_id,
-                second_practice_time_and_date_id,
-                third_practice_time_and_date_id,
-                wikipedia_page
-            ) values ((select id from circuits where circuit_identifier = ?),?,?,?,?,?,?,?,?,?,?);
-            """
-        )) {
-            preparedStatement.setString(1, raceData.getCircuitData().getCircuitIdentifier());
-            preparedStatement.setInt(2, raceData.getYear());
-            preparedStatement.setInt(3, raceData.getRound());
-            preparedStatement.setString(4, raceData.getRaceName());
-            setInt(preparedStatement, 5, raceTimeAndDateId.orElse(null));
-            setInt(preparedStatement, 6, qualifyingTimeAndDateId.orElse(null));
-            setInt(preparedStatement, 7, sprintTimeAndDateId.orElse(null));
-            setInt(preparedStatement, 8, fp1TimeAndDateId.orElse(null));
-            setInt(preparedStatement, 9, fp2TimeAndDateId.orElse(null));
-            setInt(preparedStatement, 10, fp3TimeAndDateId.orElse(null));
-            setString(preparedStatement, 11, raceData.getWikipediaUrl().getUrl());
-
-            preparedStatement.executeUpdate();
-        }
-    }
-
-    private Optional<Integer> insertTimeAndDate(final Time time, final java.util.Date date, final Connection connection) throws SQLException {
-        if (time == null && date == null) {
-            return Optional.empty();
-        }
-
-        try (final PreparedStatement preparedStatement = connection.prepareStatement(
-            "insert into time_and_dates (date, time) values (?,?);",
-            Statement.RETURN_GENERATED_KEYS
-        )) {
-            setDate(preparedStatement, 1, date);
-            setTime(preparedStatement, 2, time);
-            preparedStatement.executeUpdate();
-            final ResultSet resultSet = preparedStatement.getGeneratedKeys();
-            if (resultSet.next()) {
-                return Optional.of(resultSet.getInt(1));
-            }
-            throw new SQLException("Unable to read and return corresponding id created from background job insert");
-        }
+        executeVoidQuery(new SetLastFetchedSeasonForRaceFetchingQueryData(lastFetchedSeason));
     }
 }
---
+++ F1Info/src/main/java/f1_Info/background/fetch_races_task/fetch_races_task_procedures.sql
@@ -0,0 +1,88 @@
+drop procedure if exists tasks_get_next_season_to_fetch_for_races;
+create procedure tasks_get_next_season_to_fetch_for_races(in p_first_formula_1_race int)
+begin
+  select ifnull((
+    select
+      case when seasons.year is not null then history.season + 1 else -1 end as next_season_to_fetch
+    from
+      races_fetching_history history
+        left join seasons on seasons.year = history.season + 1
+    where
+        is_active = 'Y'
+    ), p_first_formula_1_race) as next_fetch_year;
+end;
+
+
+
+drop procedure if exists tasks_set_last_fetched_season_for_race_fetching;
+create procedure tasks_set_last_fetched_season_for_race_fetching(in p_last_fetched_season int)
+begin
+  update races_fetching_history set is_active = 'N' where is_active = 'Y';
+  insert races_fetching_history (season, is_active) values (p_last_fetched_season, 'Y');
+end;
+
+
+
+drop procedure if exists tasks_insert_race_if_not_present;
+create procedure tasks_insert_race_if_not_present(
+  in p_year int,
+  in p_round int,
+  in p_url varchar(255),
+  in p_race_name varchar(255),
+  in p_race_time time,
+  in p_race_date date,
+  in p_qualifying_time time,
+  in p_qualifying_date date,
+  in p_sprint_time time,
+  in p_sprint_date date,
+  in p_first_practice_time time,
+  in p_first_practice_date date,
+  in p_second_practice_time time,
+  in p_second_practice_date date,
+  in p_third_practice_time time,
+  in p_third_practice_date date,
+  in p_circuit_identifier varchar(255)
+)
+begin
+  declare v_exists_in_table char;
+  set v_exists_in_table := (select if(count(*) = 1, 'Y', 'N') from races where year = p_year and round = p_round);
+
+  if (v_exists_in_table = 'N') then
+    insert into races (
+      year,
+      round,
+      circuit_id,
+      name,
+      race_time_and_date_id,
+      qualifying_time_and_date_id,
+      sprint_time_and_date_id,
+      first_practice_time_and_date_id,
+      second_practice_time_and_date_id,
+      third_practice_time_and_date_id,
+      wikipedia_page
+    ) values (
+      p_year,
+      p_round,
+      (select id from circuits where circuit_identifier = p_circuit_identifier),
+      p_race_name,
+      tasks_insert_time_and_date_if_data_present(p_race_time, p_race_date),
+      tasks_insert_time_and_date_if_data_present(p_qualifying_time, p_qualifying_date),
+      tasks_insert_time_and_date_if_data_present(p_sprint_time, p_sprint_date),
+      tasks_insert_time_and_date_if_data_present(p_first_practice_time, p_first_practice_date),
+      tasks_insert_time_and_date_if_data_present(p_second_practice_time, p_second_practice_date),
+      tasks_insert_time_and_date_if_data_present(p_third_practice_time, p_third_practice_date),
+      p_url
+    );
+  end if;
+end;
+
+drop function if exists tasks_insert_time_and_date_if_data_present;
+create function tasks_insert_time_and_date_if_data_present(p_time time, p_date date) returns int
+begin
+  if (p_time is null and p_date is null) then
+    return null;
+  end if;
+
+  insert into time_and_dates (date, time) values (p_date, p_time);
+  return last_insert_id();
+end;
