9072e9ad278bb3d4d04129df916689910df7db33
Filip Petersson <zmarfan@hotmail.com>
2022-04-08 20:49:07 +0200

    Fixed issue with badly formatted response object

--- F1Info/src/main/java/f1_Info/ergast/responses/pit_stop/PitStopData.java
+++ F1Info/src/main/java/f1_Info/ergast/responses/pit_stop/PitStopData.java
@@ -1,16 +1,19 @@
 package f1_Info.ergast.responses.pit_stop;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import f1_Info.utils.DateUtils;
 import lombok.Value;
 
 import java.math.BigDecimal;
+import java.sql.Time;
+import java.text.ParseException;
 
 @Value
 public class PitStopData {
     String mDriverIdentification;
     int mLap;
     int mStop;
-    String mTime;
+    Time mTime;
     BigDecimal mDurationInSeconds;
 
     public PitStopData(
@@ -19,11 +22,11 @@ public class PitStopData {
         @JsonProperty("stop") int stop,
         @JsonProperty("time") String time,
         @JsonProperty("duration") BigDecimal durationInSeconds
-    ) {
+    ) throws ParseException {
         mDriverIdentification = driverIdentification;
         mLap = lap;
         mStop = stop;
-        mTime = time;
+        mTime = DateUtils.parseTime(time);
         mDurationInSeconds = durationInSeconds;
     }
 }
