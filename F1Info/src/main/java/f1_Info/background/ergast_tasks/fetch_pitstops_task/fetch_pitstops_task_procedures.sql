da813b8d8228e6cd0bb6f78f99428bb0bbc5148a
Filip Petersson <zmarfan@hotmail.com>
2022-04-10 20:46:18 +0200

    Fixed overcomplicated queries to reuse stuff it can

--- F1Info/src/main/java/f1_Info/background/ergast_tasks/fetch_pitstops_task/fetch_pitstops_task_procedures.sql
+++ F1Info/src/main/java/f1_Info/background/ergast_tasks/fetch_pitstops_task/fetch_pitstops_task_procedures.sql
@@ -3,25 +3,25 @@ create procedure tasks_get_next_race_to_fetch_pitstops_for(in p_first_season_wit
 begin
   declare v_last_fetched_season int;
   declare v_last_fetched_round int;
-  declare v_next_round_in_season_exists char;
-  declare v_next_season_race_exists char;
+  declare v_next_round_race_id int;
+  declare v_next_season_race_id int;
 
   set v_last_fetched_season := ifnull((select season from pit_stop_fetching_history where is_active = 'Y'), p_first_season_with_pitstop_data);
   set v_last_fetched_round := ifnull((select round from pit_stop_fetching_history where is_active = 'Y'), 0);
-  set v_next_round_in_season_exists := (select if(count(*) = 1, 'Y', 'N') from races where year = v_last_fetched_season and round = v_last_fetched_round + 1);
+  set v_next_round_race_id := (select id from races where year = v_last_fetched_season and round = v_last_fetched_round + 1);
 
-  if (v_last_fetched_season = p_first_season_with_pitstop_data and v_last_fetched_round = 0 and v_next_round_in_season_exists = 'N') then
+  if (v_last_fetched_season = p_first_season_with_pitstop_data and v_last_fetched_round = 0 and v_next_round_race_id is null) then
     call return_null();
   end if;
 
-  if (v_next_round_in_season_exists = 'Y') then
-    select v_last_fetched_season, v_last_fetched_round + 1, (select id from races where year = v_last_fetched_season and round = v_last_fetched_round + 1);
+  if (v_next_round_race_id is not null) then
+    select v_last_fetched_season, v_last_fetched_round + 1, v_next_round_race_id;
   end if;
 
-  set v_next_season_race_exists := (select if(count(*) > 0, 'Y', 'N') from races where year = v_last_fetched_season + 1 and round = 1);
+  set v_next_season_race_id := (select id from races where year = v_last_fetched_season + 1 and round = 1);
 
-  if (v_next_season_race_exists) then
-    select v_last_fetched_season + 1, 1, (select id from races where year = v_last_fetched_season + 1 and round = 1);
+  if (v_next_season_race_id is not null) then
+    select v_last_fetched_season + 1, 1, v_next_season_race_id;
   end if;
 
   call return_null();
