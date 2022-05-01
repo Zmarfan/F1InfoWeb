9e68427dd4fcd21dfa6d21360fe75449b3430a86
Filip Petersson <zmarfan@hotmail.com>
2022-05-01 22:52:35 +0200

    Added basic outline for profile navigation thingy

--- f1_info_frontend/src/core/core.module.ts
+++ f1_info_frontend/src/core/core.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 import { LoadingSpinnerComponent } from './loading/loading-spinner/loading-spinner.component';
 import { LoadingElementComponent } from './loading/loading-element/loading-element.component';
 import { MainHeaderComponent } from './navigation/main-header/main-header.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { ProfileHeaderComponent } from './navigation/profile-header/profile-header.component';
 
 @NgModule({
     declarations: [
         LoadingSpinnerComponent,
         LoadingElementComponent,
         MainHeaderComponent,
+        ProfileHeaderComponent,
     ],
     imports: [
         CommonModule,
+        FontAwesomeModule,
     ],
     exports: [
         MainHeaderComponent,
---
+++ f1_info_frontend/src/core/navigation/profile-header/profile-header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-profile-header',
+    templateUrl: './profile-header.component.html',
+    styleUrls: ['./profile-header.component.scss'],
+})
+export class ProfileHeaderComponent {
+}
