3dbd397e3611d9d422e8ad2f339636abe6f86f61
Filip Petersson <zmarfan@hotmail.com>
2022-05-01 21:56:20 +0200

    Created component for main header

--- f1_info_frontend/src/core/core.module.ts
+++ f1_info_frontend/src/core/core.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingSpinnerComponent } from './loading/loading-spinner/loading-spinner.component';
 import { LoadingElementComponent } from './loading/loading-element/loading-element.component';
+import { MainHeaderComponent } from './navigation/main-header/main-header.component';
 
 @NgModule({
     declarations: [
         LoadingSpinnerComponent,
         LoadingElementComponent,
+        MainHeaderComponent,
     ],
     imports: [
         CommonModule,
