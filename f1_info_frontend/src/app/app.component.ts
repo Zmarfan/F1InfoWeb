91ff1c57d9a6a4dece40edafbbcd00310b832e3c
Filip Petersson <zmarfan@hotmail.com>
2022-05-01 16:50:29 +0200

    Fixed formatting and removed test bed test file

--- f1_info_frontend/src/app/app.component.ts
+++ f1_info_frontend/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'f1_info_frontend';
+    title = 'f1_info_frontend';
 }
