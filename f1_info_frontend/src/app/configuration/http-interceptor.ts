26e65b6b3629bd198057158ef1f82f4a86d88f7f
Filip Petersson <zmarfan@hotmail.com>
2022-06-19 18:54:56 +0200

    Extended http interceptor to send all unauthorized to homepage

--- f1_info_frontend/src/app/configuration/http-interceptor.ts
+++ f1_info_frontend/src/app/configuration/http-interceptor.ts
@@ -1,12 +1,30 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {catchError, Observable, throwError} from 'rxjs';
+import {Session} from './session';
+import {Router} from '@angular/router';
+import {Injectable} from '@angular/core';
+import {RouteHolder} from '../routing/route-holder';
 
+@Injectable()
 export class TokenInterceptor implements HttpInterceptor {
+
+    public constructor(
+        private mRouter: Router
+    ) {
+    }
+
     public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const xhr = request.clone({
             withCredentials: true,
             setHeaders: {},
         });
-        return next.handle(xhr);
+        return next.handle(xhr).pipe(
+            catchError((error: HttpErrorResponse) => {
+                if (error.status === 401) {
+                    this.mRouter.navigateByUrl(RouteHolder.HOMEPAGE).then();
+                }
+                return throwError(() => error);
+            })
+        );
     }
 }
