a91bb6b91e5c4639fcd741c449cf8a402390c5bf
Filip Petersson <zmarfan@hotmail.com>
2022-08-08 14:17:44 +0200

    Made connection only being established once to speed up task

--- F1Info/MockDataGenerator/src/main/java/mock_data_generator/MockDataGenerator.java
+++ F1Info/MockDataGenerator/src/main/java/mock_data_generator/MockDataGenerator.java
@@ -31,13 +31,15 @@ public class MockDataGenerator {
 
     public void run() {
         try {
+            final Connection connection = getConnection();
+
             logInfo("Starting Mock Data Generator");
-            resetDatabase();
-            createTables();
-            createHelperFunctions();
-            createData();
-            createProcedures();
-            createMockData();
+            resetDatabase(connection);
+            createTables(connection);
+            createHelperFunctions(connection);
+            createData(connection);
+            createProcedures(connection);
+            createMockData(connection);
             logSuccess("Successfully reset local database!");
         } catch (final Exception e) {
             logError("Unable to finish the mock data generation!");
@@ -45,52 +47,52 @@ public class MockDataGenerator {
         }
     }
 
-    private void resetDatabase() throws SQLException, IOException {
+    private void resetDatabase(final Connection connection) throws SQLException, IOException {
         logInfo("Resetting database...");
-        runSqlStatementsFromFilePath(Path.of(RESET_DATABASE_FILE_PATH));
+        runSqlStatementsFromFilePath(connection, Path.of(RESET_DATABASE_FILE_PATH));
     }
 
-    private void createTables() throws SQLException, IOException {
+    private void createTables(final Connection connection) throws SQLException, IOException {
         logInfo("Creating Tables...");
-        runSqlFilesEndingWith("_tables.sql");
+        runSqlFilesEndingWith(connection, "_tables.sql");
     }
 
-    private void createData() throws SQLException, IOException {
+    private void createData(final Connection connection) throws SQLException, IOException {
         logInfo("Setting up Data...");
-        executeSqlFilesInDirectory(DATA_DIRECTORY);
+        executeSqlFilesInDirectory(connection, DATA_DIRECTORY);
     }
 
-    private void createHelperFunctions() throws SQLException, IOException {
+    private void createHelperFunctions(final Connection connection) throws SQLException, IOException {
         logInfo("Setting up Helper Functions...");
-        executeSqlFilesInDirectory(HELPER_FUNCTIONS_DIRECTORY);
+        executeSqlFilesInDirectory(connection, HELPER_FUNCTIONS_DIRECTORY);
     }
 
-    private void createProcedures() throws SQLException, IOException {
+    private void createProcedures(final Connection connection) throws SQLException, IOException {
         logInfo("Setting up Procedures...");
-        runSqlFilesEndingWith("_procedures.sql");
+        runSqlFilesEndingWith(connection, "_procedures.sql");
     }
 
-    private void createMockData() throws SQLException {
+    private void createMockData(final Connection connection) throws SQLException {
         logInfo("Creating mock data...");
 
         DatabaseUtil.executeQuery(getConnection(), new CreateUsersQueryData(), null, null);
     }
 
-    private void executeSqlFilesInDirectory(final String directoryPath) throws SQLException, IOException {
+    private void executeSqlFilesInDirectory(final Connection connection, final String directoryPath) throws SQLException, IOException {
         final File tablesFolder = new File(directoryPath);
         final List<Path> paths = stream(requireNonNull(tablesFolder.listFiles())).map(file -> Path.of(file.getPath())).toList();
 
         for (final Path path : paths) {
-            runSqlStatementsFromFilePath(path);
+            runSqlStatementsFromFilePath(connection, path);
         }
     }
 
-    private void runSqlStatementsFromFilePath(final Path filePath) throws SQLException, IOException {
+    private void runSqlStatementsFromFilePath(final Connection connection, final Path filePath) throws SQLException, IOException {
         logFile("Executing: " + filePath.getFileName().toString());
 
         final List<String> sqlStatements = MockDataFileReader.getSqlFileStatements(filePath);
         String executingStatement = sqlStatements.get(0);
-        try (final Statement statement = getConnection().createStatement()) {
+        try (final Statement statement = connection.createStatement()) {
             for (final String sqlStatement : sqlStatements) {
                 executingStatement = sqlStatement;
                 statement.execute(sqlStatement);
@@ -102,7 +104,7 @@ public class MockDataGenerator {
         }
     }
 
-    private void runSqlFilesEndingWith(final String ending) throws SQLException, IOException {
+    private void runSqlFilesEndingWith(final Connection connection, final String ending) throws SQLException, IOException {
         Path start = Paths.get(".");
         try (Stream<Path> stream = Files.walk(start, Integer.MAX_VALUE)) {
             List<File> proceduresSqlFiles = stream
@@ -112,7 +114,7 @@ public class MockDataGenerator {
                 .toList();
 
             for (final File file : proceduresSqlFiles) {
-                runSqlStatementsFromFilePath(file.toPath());
+                runSqlStatementsFromFilePath(connection, file.toPath());
             }
         }
     }
