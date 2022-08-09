f9d95ccec6087ab2ed8fe895e151d990764da3eb
Filip Petersson <zmarfan@hotmail.com>
2022-08-09 13:27:53 +0200

    Created main structure around race report

--- f1_info_frontend/src/app/race-report/race-report.component.ts
+++ f1_info_frontend/src/app/race-report/race-report.component.ts
@@ -1,9 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DropdownOption} from '../reports/filters/drop-down-filter/drop-down-filter.component';
 import {DropDownFilterProvider} from '../reports/filters/drop-down-filter/drop-down-filter-provider';
 import {RaceReportService} from './race-report.service';
 import {GlobalMessageService} from '../../core/information/global-message-display/global-message.service';
-import {RaceType} from '../driver-report/driver-report-data';
 import {RaceReport} from './race-report-data';
 
 @Component({
@@ -11,9 +10,10 @@ import {RaceReport} from './race-report-data';
     templateUrl: './race-report.component.html',
     styleUrls: ['./../reports/report-styling.scss'],
 })
-export class RaceReportComponent {
+export class RaceReportComponent implements OnInit {
     public seasonsOptions: DropdownOption[] = DropDownFilterProvider.createSeasonOptions();
     public filterLoading: boolean = true;
+    public loading: boolean = true;
 
     private mSelectedSeason: number = new Date().getFullYear();
     private mReportType: RaceReport = RaceReport.OVERVIEW;
@@ -22,6 +22,10 @@ export class RaceReportComponent {
         private mRaceReportService: RaceReportService,
         private mMessageService: GlobalMessageService
     ) {
+    }
+
+    public ngOnInit() {
+        this.fetchAndAssignFilterValues();
     }
 
     public seasonFilterChanged = (newSeason: number) => {
@@ -44,18 +48,22 @@ export class RaceReportComponent {
     }
 
     private runReport() {
-        //switch (this.mReportType) {
-        //case RaceReport.OVERVIEW: this.runOverviewReport(); break;
-        //case RaceReport.RACE_RESULT: this.runRaceResulTReport(); break;
-        //case RaceReport.FASTEST_LAPS: this.runFastestLapsReport(); break;
-        //case RaceReport.PIT_STOPS: this.runPitStopSReport(); break;
-        //case RaceReport.STARTING_GRID: this.runStartingGridReport(); break;
-        //case RaceReport.SPRINT: this.runSprintReport(); break;
-        //case RaceReport.SPRINT_GRID: this.runSprintGridReport(); break;
-        //case RaceReport.PRACTICE_1: this.runPractice1Report(); break;
-        //case RaceReport.PRACTICE_2: this.runPractice2Report(); break;
-        //case RaceReport.PRACTICE_3: this.runPractice3Report(); break;
-        //case RaceReport.QUALIFYING: this.runQualifyingReport(); break;
-        //}
+        switch (this.mReportType) {
+        case RaceReport.OVERVIEW: break;
+        case RaceReport.RACE_RESULT: break;
+        case RaceReport.FASTEST_LAPS: break;
+        case RaceReport.PIT_STOPS: break;
+        case RaceReport.STARTING_GRID: break;
+        case RaceReport.SPRINT: break;
+        case RaceReport.SPRINT_GRID: break;
+        case RaceReport.PRACTICE_1: break;
+        case RaceReport.PRACTICE_2: break;
+        case RaceReport.PRACTICE_3: break;
+        case RaceReport.QUALIFYING: break;
+        }
     }
+
+    private loadingCallback = (loading: boolean) => {
+        this.loading = loading;
+    };
 }
