0ce17146c6b7044cf3e00eb3e5574a1492a2258c
Filip Petersson <zmarfan@hotmail.com>
2022-08-27 18:07:32 +0200

    Added close button to website info

--- f1_info_frontend/src/core/navigation/profile-header/website-info/website-info.component.ts
+++ f1_info_frontend/src/core/navigation/profile-header/website-info/website-info.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatDialogRef} from '@angular/material/dialog';
 
 @Component({
     selector: 'app-website-info',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
     styleUrls: ['./website-info.component.scss'],
 })
 export class WebsiteInfoComponent {
+    public constructor(
+        private mDialogRef: MatDialogRef<WebsiteInfoComponent>
+    ) {
+    }
 
+    public closePopup() {
+        this.mDialogRef.close();
+    }
 }
