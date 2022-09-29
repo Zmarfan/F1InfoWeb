6e4c58e04681c1f12d26325d616049f5362c21f5
Filip Petersson <zmarfan@hotmail.com>
2022-09-29 14:30:53 +0200

    Fixed issue where users own display name was displayed instead of search user's in search friend

--- F1Info/src/main/java/f1_Info/entry_points/friends/command/search_friend_command/search_friend_command_procedures.sql
+++ F1Info/src/main/java/f1_Info/entry_points/friends/command/search_friend_command/search_friend_command_procedures.sql
@@ -2,11 +2,11 @@ drop procedure if exists get_search_friend_info;
 create procedure get_search_friend_info(in p_user_id int, in p_search_user_id int)
 begin
   select
-    user_info.display_name,
+    search_user_info.display_name,
     shared_friends.friends_in_common,
     ifnull(friend_status.friend_status, 'not_friend') as friend_status
   from
-    latest_user_information_v user_info
+    users
     inner join (
       select
         users.id as user_id,
@@ -17,8 +17,9 @@ begin
         left join latest_user_friends_v search_friends on search_friends.user_id = p_search_user_id
       where
         users.id = p_user_id
-    ) shared_friends on shared_friends.user_id = user_info.user_id
-    left join latest_user_friend_status_v friend_status on friend_status.user_id = user_info.user_id and friend_status.friend_user_id = p_search_user_id
+    ) shared_friends on shared_friends.user_id = users.id
+    left join latest_user_information_v search_user_info on search_user_info.user_id = p_search_user_id
+    left join latest_user_friend_status_v friend_status on friend_status.user_id = users.id and friend_status.friend_user_id = search_user_info.user_id
   where
-    user_info.user_id = p_user_id;
+    users.id = p_user_id;
 end;
